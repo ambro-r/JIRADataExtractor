22e54516ec495793f2345977d45daae076ecd516
ambro-r <rrambrosio@gmail.com>
2020-05-12 20:36:01 +0200

    Added a JSON Object map for fields.

---
+++ Objects/Field.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JIRADataExtractor.Objects
+{
+    class Field
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("key")]
+        public string Key { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("custom")]
+        public bool Custom { get; set; }
+
+        public override string ToString()
+        {
+            return new StringBuilder("[Field:")
+                .Append(", id=").Append(Id)
+                .Append(", Key=").Append(Key)
+                .Append(", name=").Append(Name)
+                .Append(", custom=").Append(Custom)
+                .Append("]").ToString();
+        }
+    }
+}
