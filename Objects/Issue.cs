286b1413109f1d4812d8105ffadd78bbb5df97e6
ambro-r <rrambrosio@gmail.com>
2020-05-12 20:37:39 +0200

    Modifed the Issue class so that it can be mapped to the JSON response (Need to still cater for custom fields)

--- Objects/Issue.cs
+++ Objects/Issue.cs
@@ -2,25 +2,49 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using Newtonsoft.Json;
 
 namespace JIRADataExtractor
 {
+    [JsonObject]
     class Issue
     {
+        [JsonProperty("created")]
         public DateTimeOffset? CreatedDate { get; set; }
+        [JsonProperty("id")]
         public long Id { get; set; }
+        [JsonProperty("key")]
         public string Key { get; set; }
-        public string Parent { get; set; }
+        [JsonProperty("fields/parent/key")]
+        public string ParentKey { get; set; }
+        [JsonProperty("fields/parent/fields/summary")]
+        public string ParentName { get; set; }
+        [JsonProperty("fields/parent/fields/issuetype/name")]
+        public string ParentType { get; set; }
+        [JsonProperty("fields/priority/name")]
         public string Priority { get; set; }
+        [JsonProperty("fields/project/key")]
+        public string ProjectKey { get; set; }
+        [JsonProperty("fields/project/name")]
+        public string ProjectName { get; set; }
+        [JsonProperty("fields/project/projectCategory/name")]
+        public string ProjectCategory { get; set; }
+        [JsonProperty("fields/summary")]   
         public string Summary { get; set; }
+        [JsonProperty("fields/status/name")]
         public string Status { get; set; }
+        [JsonProperty("fields/issuetype/name")]
         public string Type { get; set; }
         public Dictionary<String, String> CustomFields { get; set; } 
 
         public Issue()
         {
             CustomFields = new Dictionary<string, string>();
-            Parent = "";
+        }
+
+        private string toCSV()
+        {
+            return "";
         }
 
         public override string ToString() {
@@ -31,7 +55,8 @@ namespace JIRADataExtractor
                 .Append(", Priority=").Append(Priority)
                 .Append(", Status=").Append(Status)
                 .Append(", Type=").Append(Type)
-                .Append(", Parent=").Append(Parent)
+                .Append(string.Format(", Parent(key={0};type={1};name:{2})", ParentKey, ParentType, ParentName))
+                .Append(string.Format(", Project(key={0};name:{1};category:{2})", ProjectKey, ProjectName, ProjectCategory))
                 .Append(", Summary=").Append(Summary)
                 .Append(", CustomFields=(").Append(string.Join("'", CustomFields.Select(a => $"{a.Key}:{a.Value}"))).Append(")")
                 .Append("]").ToString();
