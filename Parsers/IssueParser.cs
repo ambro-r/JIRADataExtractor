72fc060cf8d978b5dc6946c55a3b7f95cf8932f2
ambro-r <rrambrosio@gmail.com>
2020-05-12 20:36:58 +0200

    Added a parser to get details regarding a specific issue from JIRA

---
+++ Parsers/IssueParser.cs
@@ -0,0 +1,54 @@
+﻿using JIRADataExtractor.Converters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JIRADataExtractor.Parsers
+{
+    class IssueParser
+    {
+        private JIRAConnectionHandler JIRAConnectionHandler;
+        private IssueParser() { }
+        public IssueParser(JIRAConnectionHandler jIRAConnectionHandler)
+        {
+            JIRAConnectionHandler = jIRAConnectionHandler;
+        }
+        public IssueParser(String userName, String password, String baseURL)
+        {
+            JIRAConnectionHandler = new JIRAConnectionHandler(userName, password, baseURL);
+        }
+
+        public Issue GetIssue(long issueID)
+        {
+            Log.Information("Getting issue with id {issueID}", issueID);
+            return ParseJSON(Execute(issueID.ToString()));
+        }
+
+        public Issue GetIssue(string issueKey)
+        {
+            Log.Information("Getting issue with key {issueKey}", issueKey);
+            return ParseJSON(Execute(issueKey));
+        }
+
+        private string Execute(string issueIdOrKey)
+        {
+            return JIRAConnectionHandler.execute("/rest/api/3/issue/" + issueIdOrKey);
+        }
+
+        private Issue ParseJSON(String jSONResponse)
+        { 
+            if(Log.IsEnabled(LogEventLevel.Debug))
+            {
+                Log.Debug("{jsonData}", JObject.Parse(jSONResponse).ToString());
+            }
+            var settings = new JsonSerializerSettings();
+            settings.Converters.Add(new NestedJSONConverter<Issue>());
+            return JsonConvert.DeserializeObject<Issue>(jSONResponse, settings);
+        }
+
+    }
+}
