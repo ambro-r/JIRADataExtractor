ae37e4cf006eb5d8db67387e26041a433c717794
ambro-r <rrambrosio@gmail.com>
2020-05-12 20:42:09 +0200

    Renamed SprintIssues to SprinParser (need to re-think and re-work this class)

--- Parsers/SprintParser.cs
+++ Parsers/SprintParser.cs
@@ -8,53 +8,57 @@ using System.Text;
 
 namespace JIRADataExtractor
 {
-    class SprintIssues
+    class SprintParser
     {
         private JIRAConnectionHandler JIRAConnectionHandler;
-        private SprintIssues() { }
-
-        public SprintIssues(String userName, String password, String baseURL)
+        private SprintParser() { }
+        public SprintParser(JIRAConnectionHandler jIRAConnectionHandler)
+        {
+            JIRAConnectionHandler = jIRAConnectionHandler;
+        }
+        public SprintParser(String userName, String password, String baseURL)
         {
             JIRAConnectionHandler = new JIRAConnectionHandler(userName, password, baseURL);
         }
 
-        public List<Issue> getSprintIssues(long sprintID)
+        public List<Issue> GetSprintIssues(long sprintID)
         {
-            return getSprintIssues(sprintID, new string[] { });
+            return GetSprintIssues(sprintID, new string[] { });
         }
 
-        public List<Issue> getSprintIssues(long sprintID, String[] customFields)
+        public List<Issue> GetSprintIssues(long sprintID, String[] customFields)
         {
             Log.Information("Getting issues for sprint with ID {sprintID}", sprintID);
-            return parseJSON(execute("/rest/agile/1.0/sprint/" + sprintID, customFields), customFields);
+            return ParseJSON(Execute("/rest/agile/1.0/sprint/" + sprintID, customFields), customFields);
         }
 
-        public List<Issue> getSprintIssues(String sprintName)
+        public List<Issue> GetSprintIssues(String sprintName)
         {
-            return getSprintIssues(sprintName, new string[] { });
+            return GetSprintIssues(sprintName, new string[] { });
         }
 
-        public List<Issue> getSprintIssues(String sprintName, String[] customFields)
+        public List<Issue> GetSprintIssues(String sprintName, String[] customFields)
         {
             Log.Information("Getting issues for sprint with name {sprintName}", sprintName);
-            return parseJSON(execute("/rest/api/3/search?jql=Sprint=\"" + sprintName + "\"", customFields), customFields);
+            return ParseJSON(Execute("/rest/api/3/search?jql=Sprint=\"" + sprintName + "\"", customFields), customFields);
         }
 
-        private string execute(String unfilteredURI, String[] customFields)
+        private string Execute(String unfilteredURI, String[] customFields)
         {
-            string fields = "&fields=issuetype,parent,summary,created,priority,status";
+            string fields = "&fields=issuetype,parent,summary,created,priority,status,project";
             foreach (string field in customFields)
             {
                 fields += "," + field;
             }
             Log.Debug("Field filter applied is {fields}", fields);
-            return JIRAConnectionHandler.execute(unfilteredURI + fields);
+            return JIRAConnectionHandler.execute(unfilteredURI);// + fields);
         }
 
-        private List<Issue> parseJSON(String jSONResponse, String[] customFields)
+        private List<Issue> ParseJSON(String jSONResponse, String[] customFields)
         {
             List<Issue> issues = new List<Issue>();
             var jsonData = JObject.Parse(jSONResponse);
+            Console.WriteLine(jsonData.ToString());
             foreach (JObject issueJSON in jsonData["issues"])
             {
                 Issue issue = new Issue();
@@ -67,12 +71,7 @@ namespace JIRADataExtractor
                 issue.Priority = (string)fieldJSON["priority"]["name"];
                 issue.Status = (string)fieldJSON["status"]["name"];
                 issue.Type = (string)fieldJSON["issuetype"]["name"];
-                
 
-                if (fieldJSON.ContainsKey("parent"))
-                {
-                    issue.Parent = (string)fieldJSON["parent"]["key"];
-                }
 
                 Dictionary<string, string> custom = new Dictionary<string, string>();
                 foreach (string field in customFields)
@@ -80,8 +79,8 @@ namespace JIRADataExtractor
                     custom.Add(field, (string)fieldJSON[field]);
                 }
                 issue.CustomFields = custom;
+
                 issues.Add(issue);
-                Console.WriteLine(issue.ToString());
             }
             Log.Information("{issueCount} issues found in sprint.", issues.Count);
             return issues;
