5cdf512d75c01057932b8d249ff62f32bb525eb1
ambro-r <rrambrosio@gmail.com>
2020-05-19 18:45:52 +0200

    Added additional constants for JQLSearchResults

--- Constants/JQLSearchResult.cs
+++ Constants/JQLSearchResult.cs
@@ -6,9 +6,10 @@ namespace JIRADataExtractor.Constants
 {
     class JQLSearchResult
     {
-
+        public static string IS_LAST = "isLast";
         public static string MAX_RESULTS = "maxResults";
         public static string START_AT = "startAt";
         public static string TOTAL = "total";
+        public static string VALUES = "values";
     }
 }
