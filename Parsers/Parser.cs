26911cc856250081abe1d7f8bebb2b1ec0c1a6eb
ambro-r <rrambrosio@gmail.com>
2020-05-19 12:12:52 +0200

    Added super class for Parsers

---
+++ Parsers/Parser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JIRADataExtractor.Parsers
+{
+    class Parser
+    {
+        protected JIRAConnectionHandler JIRAConnectionHandler;
+        protected Parser() { }
+        protected Parser(JIRAConnectionHandler jIRAConnectionHandler)
+        {
+            JIRAConnectionHandler = jIRAConnectionHandler;
+        }
+        protected Parser(String userName, String password, String baseURL)
+        {
+            JIRAConnectionHandler = new JIRAConnectionHandler(userName, password, baseURL);
+        }
+    }
+}
