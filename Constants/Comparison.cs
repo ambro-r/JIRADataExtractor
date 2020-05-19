325c49b18ad1a62fc4bfb46200c53123adfe04d0
ambro-r <rrambrosio@gmail.com>
2020-05-19 10:58:02 +0200

    Added object to support JQLFilter parameters

---
+++ Constants/Comparison.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JIRADataExtractor.Constants
+{
+    class Operators
+    {
+    }
+}
---
+++ Objects/JQLFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JIRADataExtractor.Objects
+{
+    class SearchElement
+    {
+        public enum OPERATORS { OR, AND };
+
+        public OPERATORS Operator { get; set; }
+
+        //   project = PHX AND updated >= 2020-05-11 AND updated <= 2020-05-23 order by created DESC
+    }
+}
