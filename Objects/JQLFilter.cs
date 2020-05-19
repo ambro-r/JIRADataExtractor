2f460be65d50cc74b303721c8aa3dffad39148dd
ambro-r <rrambrosio@gmail.com>
2020-05-19 16:17:30 +0200

    Added some default constructors

--- Objects/JQLFilter.cs
+++ Objects/JQLFilter.cs
@@ -8,9 +8,25 @@ namespace JIRADataExtractor.Objects
     class JQLFilter
     {
         // example: AND project = PHX 
-        public Gate gate { get; set; } // AND
-        public string field { get; set; } // Project
-        public Comparison comparison { get; set; } // =
-        public string value { get; set; } // PHX
+        public Gate Gate { get; set; } // AND
+        public string Field { get; set; } // Project
+        public Comparison Comparison { get; set; } // =
+        public string Value { get; set; } // PHX
+
+        public JQLFilter(string field, Comparison comparison, string value)
+        {
+            Field = field;
+            Comparison = comparison;
+            Value = value;
+        }
+
+        public JQLFilter(string field, Comparison comparison, string value, Gate gate)
+        {
+            Field = field;
+            Comparison = comparison;
+            Value = value;
+            Gate = gate;
+        }
+
     }
 }
