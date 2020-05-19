0f559eecb1cccb39414fe319c72f6537c0ddeb22
ambro-r <rrambrosio@gmail.com>
2020-05-19 18:46:10 +0200

    Added JSON mapping objects for Board and Sprint

---
+++ Objects/Board.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JIRADataExtractor.Objects
+{
+    class Board
+    {
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("location/projectName")]
+        public string ProjectName { get; set; }
+
+        [JsonProperty("location/projectKey")]
+        public string ProjectKey { get; set; }
+
+        public override string ToString()
+        {
+            return new StringBuilder("[Board:")
+                .Append(", id=").Append(Id)
+                .Append(", name=").Append(Name)
+                .Append(", projectName=").Append(ProjectName)
+                .Append(", projectKey=").Append(ProjectKey)
+                .Append("]").ToString();
+        }
+    }
+}
---
+++ Objects/Sprint.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JIRADataExtractor.Objects
+{
+    class Sprint
+    {
+
+       [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("startDate")]
+        public DateTime StartDate { get; set; }
+
+        [JsonProperty("endDate")]
+        public DateTime EndDate { get; set; }
+
+        [JsonProperty("completeDate")]
+        public DateTime CompleteDate { get; set; }
+    }
+}
