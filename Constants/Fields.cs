a864ec14d31c9d1a0405f3d1ef1c1698269b3ed9
ambro-r <rrambrosio@gmail.com>
2020-05-26 08:46:32 +0200

    Added another constants class to store "friendly" field names

---
+++ Constants/Fields.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JIRADataExtractor.Constants
+{
+    class Fields
+    {
+        public static string STORY_POINTS = "Story point estimate";
+    }
+}
