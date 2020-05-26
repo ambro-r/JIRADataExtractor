2b49f40c237cc553c0b62bd2d8264b91678744ba
ambro-r <rrambrosio@gmail.com>
2020-05-26 08:46:08 +0200

    Added custom exception for workers

---
+++ Exceptions/WorkerInitializationException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace JIRADataExtractor.Exceptions
+{
+    class WorkerInitializationException : Exception
+    {
+        public WorkerInitializationException() : base(String.Format("Unable to initialize worker {0}", GetThrowingClass()))
+        {
+        }
+
+        public WorkerInitializationException(string message) : base(String.Format("Unable to initialize worker {0}: {1}", GetThrowingClass(), message))
+        {
+        }
+
+        private static string GetThrowingClass()
+        {
+            System.Diagnostics.StackTrace stackTrace = new System.Diagnostics.StackTrace();
+            return stackTrace.GetFrame(2).GetMethod().ReflectedType.Name;
+        }
+
+    }
+}
