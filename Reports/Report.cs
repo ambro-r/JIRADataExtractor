3587cc6f1a145433087651fb1443449229fba453
ambro-r <rrambrosio@gmail.com>
2020-06-07 17:52:41 +0200

    Added Sprint Report and started building out method to report on the last closed sprint in a board.

---
+++ Reports/Report.cs
@@ -0,0 +1,20 @@
+﻿using JIRADataExtractor.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JIRADataExtractor.Reports
+{
+
+    class Report
+    {
+
+        protected JIRAConnectionHandler JIRAConnectionHandler;
+        protected Report() { }
+        protected Report(JIRAConnectionHandler jIRAConnectionHandler)
+        {
+            JIRAConnectionHandler = jIRAConnectionHandler;
+        }
+
+    }
+}
---
+++ Reports/SprintReport.cs
@@ -0,0 +1,42 @@
+﻿using JIRADataExtractor.Handlers;
+using JIRADataExtractor.Objects;
+using JIRADataExtractor.Parsers;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JIRADataExtractor.Reports
+{
+    class SprintReport : Report
+    {
+        public SprintReport(JIRAConnectionHandler jIRAConnectionHandler) : base(jIRAConnectionHandler)
+        {
+        }
+
+
+        public void LastCloseSprintReport(long boardID)
+        {
+            BoardParser boardParser = new BoardParser(JIRAConnectionHandler);
+            Board board = boardParser.GetBoard(boardID);
+            List<Sprint> sprints = boardParser.GetSprints(board);
+            Sprint lastClosedSprint = null;
+            foreach (Sprint sprint in sprints)
+            {
+                if("closed".Equals(sprint.State.ToLower())) {
+                    if (lastClosedSprint == null || (lastClosedSprint.CompleteDate < sprint.CompleteDate)) {
+                        lastClosedSprint = sprint;
+                    }
+                }                
+            }
+            if(lastClosedSprint != null)
+            {
+                Log.Debug("Last close sprint: {lastClosedSprint}", lastClosedSprint.ToString());
+            } else
+            {
+                Log.Information("No closed sprints found for board \"{name}\".", board.Name);
+            }
+        }
+
+    }
+}
