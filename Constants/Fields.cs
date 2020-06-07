0071e40908aadb649d5e564911ac5bc3a6fbb35a
ambro-r <rrambrosio@gmail.com>
2020-06-07 18:29:43 +0200

    Added some field constants.

--- Constants/Fields.cs
+++ Constants/Fields.cs
@@ -6,6 +6,10 @@ namespace JIRADataExtractor.Constants
 {
     class Fields
     {
+        public static string CUSTOM_SPRINT = "customfield_10016";
+
+        public static string PROJECT = "project";
         public static string STORY_POINTS = "Story point estimate";
+        public static string UPDATED = "updated";
     }
 }
